a1fe15642ca6997b5101067915c5f2526b845237
Woojin Lee <dev.woojin.lee@gmail.com>
2024-03-21 15:39:37 +0900

    (range-sum-of-bst) add bfs solution

---
+++ 52-range-sum-of-bst/52-range-sum-of-bst-bfs.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+import collections
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        queue, sum = collections.deque([root]), 0
+
+        # BFS
+        # 큐에 다음에 방문할 노드를 담는다
+        while queue:
+            node = queue.popleft()
+            if node:
+                if node.val > low:
+                    queue.append(node.left)
+                if node.val < high:
+                    queue.append(node.right)
+                if low <= node.val <= high:
+                    sum += node.val
+
+        return sum
