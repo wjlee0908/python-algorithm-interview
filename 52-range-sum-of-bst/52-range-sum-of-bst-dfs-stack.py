976b43d21aecf07b83c747e26890fcd17926bdf0
Woojin Lee <dev.woojin.lee@gmail.com>
2024-03-21 15:32:19 +0900

    (range-sum-of-bst) add dfs stack solution

---
+++ 52-range-sum-of-bst/52-range-sum-of-bst-dfs-stack.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        stack, sum = [root], 0
+
+        # 스택에 다음에 탐색할 노드를 append하며 DFS한다
+        while stack:
+            node = stack.pop()
+            if node:
+                if node.val > low:
+                    stack.append(node.left)
+                if node.val < high:
+                    stack.append(node.right)
+                if low <= node.val <= high:
+                    sum += node.val
+
+        return sum
